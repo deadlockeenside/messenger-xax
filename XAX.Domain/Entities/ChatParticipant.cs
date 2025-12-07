4b8600ae43e0c7413be64c5708fdea87bb5bde52
deadlockeenside <deadlockeenside@proton.me>
2025-12-07 21:23:08 +0300

    Add chat participant entity

---
+++ XAX.Domain/Entities/ChatParticipant.cs
@@ -0,0 +1,10 @@
+﻿using XAX.Domain.ValueObjects;
+
+namespace XAX.Domain.Entities;
+
+public class ChatParticipant
+{
+    public required Id Id { get; init; }
+    public required Id ChatId { get; init; }
+    public required Id UserId { get; init; }
+}
