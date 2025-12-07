e0b9bbccbbe5881449fe1c6ed1be83db5e59fff2
deadlockeenside <deadlockeenside@proton.me>
2025-12-07 21:35:45 +0300

    Add message entity

---
+++ XAX.Domain/Entities/Message.cs
@@ -0,0 +1,12 @@
+﻿using XAX.Domain.ValueObjects;
+
+namespace XAX.Domain.Entities;
+
+public class Message
+{
+    public required Id Id { get; init; }
+    public required Id ChatId { get; init; }
+    public required Id SenderId { get; init; }
+    public required DateTime SendedAt { get; init; }
+    public required Text Text { get; init; }
+}
---
+++ XAX.Domain/ValueObjects/Text.cs
@@ -0,0 +1,19 @@
+﻿namespace XAX.Domain.ValueObjects;
+
+public record Text
+{
+    public const short MAX_LENGTH = 1024;
+
+    public Text(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentNullException("Текст не может быть пустым.");
+
+        if (value.Length > MAX_LENGTH)
+            throw new ArgumentException($"Длина текста не может быть больше {MAX_LENGTH} символов.");
+
+        Value = value;
+    }
+
+    public string Value { get; init; }
+}
